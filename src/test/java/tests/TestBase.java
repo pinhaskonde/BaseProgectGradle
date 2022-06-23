aa6f0ac0bd20dc4c5b53c3352f3fc04e7c822641
pinhaskonde <pinhas.konde@gmail.com>
2022-06-23 17:26:55 +0300

    Loggers Basic - added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,25 +2,34 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
+import pages.HomePage;
 import pages.PageBase;
 
 public class TestBase {
+    protected HomePage homePage;
 
-    WebDriver wd;
+    Logger logger = LoggerFactory.getLogger(HomePage.class);
+
+    protected WebDriver wd;
 
     @BeforeSuite
     public void init(){
         wd = new ChromeDriver();
         wd.manage().window().maximize();
+
+        homePage = PageFactory.initElements(wd,HomePage.class);
+        logger.info("Start method -->   ");
     }
 
     @AfterMethod
     public void tearDown(){
-
+        logger.info("'wd.quit' - started!");
+        wd.quit();
     }
 
 
--- src/test/java/tests/TestClass.java
+++ src/test/java/tests/TestClass.java
@@ -3,21 +3,23 @@ package tests;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
-public class TestClass extends TestBase{
+import java.lang.reflect.Method;
+
+public class TestClass extends TestBase {
 
     String webSite = "https://www.google.com/";
 
     @Test
-    public void f1(){
+    public void f1(Method m) {
         wd.get(webSite);
+        logger.info(">>>    Logger from method  -   " + m.getName());
     }
 
-    @AfterSuite
-    public void tearDown(){
-
-        wd.quit();
+    String youtoobe = "https://www.youtube.com/";
+    @Test
+    public void f2(Method m) {
+        wd = new ChromeDriver();
+        wd.get(youtoobe);
+        logger.info(">>>    Logger from method  -   " + m.getName());
     }
-
-
-
 }
